d8acc3ab6a25e08920f6f8c1b948d0cd0ad20b2d
Jacobi <whyayala@live.com>
2023-03-20 18:06:56 +0000

    Add dim queries and update wishlist.

---
+++ dim-queries/favorite-armor.sql
@@ -0,0 +1,7 @@
+(basestat:total:>=68 (maxbasestatvalue:any or basestat:any:>=19))
+    or stat:hunterpve+discipline+intellect+strength:>=66
+    or stat:hunterpvp+discipline+intellect+strength:>=66
+    or stat:titanpve+discipline+intellect+strength:>=66
+    or stat:titanpvp+discipline+intellect+strength:>=66
+    or stat:warlockpve+discipline+intellect+strength:>=66 
+    or stat:warlockpvp+discipline+intellect+strength:>=66
---
+++ dim-queries/keep-armor.sql
@@ -0,0 +1,9 @@
+basestat:total:>=68 
+    or (basestat:total:>=62 maxbasestatvalue:any) 
+    or (basestat:total:>=65 basestat:any:>=19)
+    or stat:hunterpve+discipline+intellect+strength:>=61 
+    or stat:hunterpvp+discipline+intellect+strength:>=61 
+    or stat:titanpve+discipline+intellect+strength:>=61 
+    or stat:titanpvp+discipline+intellect+strength:>=61 
+    or stat:warlockpve+discipline+intellect+strength:>=61 
+    or stat:warlockpvp+discipline+intellect+strength:>=61 
