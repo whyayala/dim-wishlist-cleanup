c32a1e9b7260fd4a393960533b819e53a09e3303
Jacobi <whyayala@live.com>
2023-03-27 09:19:04 -0500

    Update favorite-armor.sql

--- dim-queries/favorite-armor.sql
+++ dim-queries/favorite-armor.sql
@@ -1,7 +1,1 @@
-(basestat:total:>=68 (maxbasestatvalue:any or basestat:any:>=19))
-    or stat:hunterpve+discipline+intellect+strength:>=66
-    or stat:hunterpvp+discipline+intellect+strength:>=66
-    or stat:titanpve+discipline+intellect+strength:>=66
-    or stat:titanpvp+discipline+intellect+strength:>=66
-    or stat:warlockpve+discipline+intellect+strength:>=66 
-    or stat:warlockpvp+discipline+intellect+strength:>=66
+(basestat:total:>=68 (maxbasestatvalue:any or basestat:any:>=23))
