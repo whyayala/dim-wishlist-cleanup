ea255df4e8e4ce6d7cf40ce2119cd6d7d9d39f2c
Jacobi <whyayala@live.com>
2023-04-17 16:21:00 +0000

    Update queries and wishlist

--- dim-queries/favorite-armor.sql
+++ dim-queries/favorite-armor.sql
@@ -1,10 +1,4 @@
 basestat:total:>=68 (
     maxbasestatvalue:any 
-    or basestat:any:>=23
-    or stat:hunterpve:>=28
-    or stat:hunterpvp:>=28
-    or stat:titanpve:>=28
-    or stat:titanpvp:>=28
-    or stat:warlockpve:>=28
-    or stat:warlockpvp:>=28
+    or basestat:any:>=28
 )
--- dim-queries/keep-armor.sql
+++ dim-queries/keep-armor.sql
@@ -1,14 +1,13 @@
 basestat:total:>=68 
-    or (basestat:total:>=62 maxbasestatvalue:any) 
-    or (basestat:total:>=65
-        (
-            basestat:any:>=19
-            or stat:hunterpve:>=26
-            or stat:hunterpvp:>=26
-            or stat:titanpve:>=26
-            or stat:titanpvp:>=26
-            or stat:warlockpve:>=26
-            or stat:warlockpvp:>=26
-            
+    or (
+        basestat:total:>=65 (
+            maxbasestatvalue:any 
+            or basestat:any:>=23 
+            or stat:hunterpve:>=30 
+            or stat:hunterpvp:>=30 
+            or stat:titanpve:>=30 
+            or stat:titanpvp:>=30 
+            or stat:warlockpve:>=30 
+            or stat:warlockpvp:>=30
         )
     )
