3d124c4b5eb8ffd556a69ed0728d22d5bb682715
Jacobi <whyayala@live.com>
2023-11-20 21:34:37 +0000

    Update armor query.

--- dim-queries/keep-armor.sql
+++ dim-queries/keep-armor.sql
@@ -4,11 +4,10 @@ basestat:total:>=68
             maxbasestatvalue:any 
             or basestat:any:>=23 
             or stat:hunterpve:>=30 
-            or stat:hunterpvp:>=30 
-            or stat:titanpve:>=30 
-            or stat:titanpvp:>=30 
-            or stat:warlockpve:>=30 
-            or stat:warlockpvp:>=30
+            or stat:hunterpvp:>=30
+            or stat:hunterlowmob:>=30
+            or stat:titan:>=30 
+            or stat:warlock:>=30 
         )
     )
     or (
@@ -16,12 +15,11 @@ basestat:total:>=68
         (
             maxbasestatvalue:any 
             or basestat:any:>=20
+            or stat:hunterlowmob:>=27
             or stat:hunterpve:>=27 
             or stat:hunterpvp:>=27 
-            or stat:titanpve:>=27
-            or stat:titanpvp:>=27
-            or stat:warlockpve:>=27
-            or stat:warlockpvp:>=27
+            or stat:titan:>=27
+            or stat:warlock:>=27
         )
     )
     or (
