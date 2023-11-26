c15e80fe4435e4d9b92cbaf0164220742be673cd
Jacobi <whyayala@live.com>
2023-11-26 18:49:05 +0000

    Remove untagged rolls and consolidate Aegis list.

--- src/structs/wishlist.rs
+++ src/structs/wishlist.rs
@@ -54,6 +54,7 @@ fn is_not_great(notes_string: &str) -> bool {
 
 fn is_desirable_roll(tags_string: &str, notes_string: &str, pair: &Pair<Rule>) -> bool {
     pair.as_rule() == Rule::roll
+        && !tags_string.is_empty()
         && !is_controller_specific(tags_string)
         && !is_not_great(notes_string)
         && !notes_string.to_lowercase().contains("pandapaxxy")
