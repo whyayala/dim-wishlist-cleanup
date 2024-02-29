54d6700a25fd72d2cfa05467931e643ae7b06416
Jacobi <whyayala@live.com>
2024-02-29 20:37:49 +0000

    Add some test logic.

--- src/structs/weapon_roll.rs
+++ src/structs/weapon_roll.rs
@@ -6,6 +6,24 @@ fn explode(string: &str, delimiter: &str) -> Vec<String> {
     vec_of_strings
 }
 
+fn filter_redundant_frenzy_combos(weapon_rolls: Vec<WeaponRoll>) -> Vec<WeaponRoll> {
+    let filtered_weapon_rolls = weapon_rolls
+        .into_iter()
+        .filter(|weapon_roll| is_redundant_frenzy_combo(&weapon_roll.perks))
+        .collect();
+    filtered_weapon_rolls
+}
+
+fn is_redundant_frenzy_combo(perks: &Vec<String>) -> bool {
+    let frenzy_id = &"4104185692".to_string();
+    let surplus_id = &"3436462433".to_string();
+    let flared_magwell_id = &"3230963543".to_string();
+    let tactical_mag_id = &"106909392".to_string();
+    perks.contains(frenzy_id) 
+        && (perks.contains(surplus_id) || perks.contains(flared_magwell_id) || perks.contains(tactical_mag_id))
+
+}
+
 #[derive(Clone)]
 pub struct WeaponRoll {
     pub item_id: String,
@@ -18,6 +36,10 @@ impl WeaponRoll {
         for item in exploded_text {
             self.perks.push(item);
         }
+    }
+
+    pub fn is_bad_perk_combo(&mut self) -> bool {
+        is_redundant_frenzy_combo(&self.perks)
     }
 
     pub fn new() -> WeaponRoll {
