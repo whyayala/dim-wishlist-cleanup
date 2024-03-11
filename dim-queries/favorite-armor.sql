26cfa005cfe1d7f00fb0d7e6002cd58bb15df057
Jacobi <whyayala@live.com>
2024-03-11 01:46:07 +0000

    Update dim queries

--- dim-queries/favorite-armor.sql
+++ dim-queries/favorite-armor.sql
@@ -1,12 +1,15 @@
 (
     basestat:total:>=68 (
         maxbasestatvalue:any 
+        or basestat:any:>=30
+    )
+)
+or (
+    (modslot:artifice or source:raid) basestat:total:>=66 (
+        maxbasestatvalue:any 
         or basestat:any:>=28
     )
 )
 or (
-    (modslot:artifice or source:raid) basestat:total:>=65 (
-        maxbasestatvalue:any 
-        or basestat:any:>=25
-    )
+    (source:raid or source:ironbanner or modslot:artifice) is:classitem
 )
