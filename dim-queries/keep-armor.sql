adbf9e2e6c6cafa8d600e729b5aad807ef8bde9b
Jacobi <whyayala@live.com>
2025-08-18 09:29:06 -0500

    Update keep-armor.sql

--- dim-queries/keep-armor.sql
+++ dim-queries/keep-armor.sql
@@ -2,19 +2,7 @@ basestat:total:>=68
     or (
         basestat:total:>=65 (
             maxbasestatvalue:any 
-            or basestat:any:>=27
-            or stat:hunterpve:>=30 
-            or stat:hunterpvp:>=30 
-            or stat:lowmob:>=32
+            or stat:dps:>30 
+            or stat:pvp:>30 
         )
-    ) 
-    or (
-        (modslot:artifice or source:raid) basestat:total:>=63
-        (
-            maxbasestatvalue:any 
-            or basestat:any:>=25
-            or stat:hunterpve:>=28 
-            or stat:hunterpvp:>=28
-            or stat:lowmob:>=30
-        )
-    ) 
+    )
